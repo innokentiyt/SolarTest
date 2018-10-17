58b066130f72344ce118ad5a47f92e870eb2d0c2
therearenospoon@gmail.com <gG8aH7bR6com>
2018-10-18 08:20:31 +0900

    add Body

--- core/src/com/solar/game/Body.java
+++ core/src/com/solar/game/Body.java
@@ -1,4 +1,64 @@
 package com.solar.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Body {
+    private String textureName;
+    private float imgRotationSpeed;
+    private float distance;
+    private float diameter;
+    private float angle;
+    private float movingSpeed;
+    private final Body parent;
+    private final List<Body> childs = new ArrayList<Body>();
+    private Texture texture;
+    private Image image;
+
+    //конструктор солнца
+    public Body() {
+        this.textureName = "badlogic.jpg";
+        this.imgRotationSpeed = 2;
+        this.distance = 0;
+        this.diameter = 80;
+        this.angle = 0;
+        this.movingSpeed = 0;
+        this.parent = null;
+        this.texture = new Texture(Gdx.files.internal(textureName));
+        this.image = new Image(this.texture);
+        this.image.setWidth(this.diameter);
+        this.image.setHeight(this.diameter);
+        this.image.setPosition(SolarSystemTest.WIDTH/2 - diameter/2, SolarSystemTest.HEIGHT/2 - diameter/2);
+        this.image.setOrigin(-300, -300);
+        this.image.setRotation(-30);
+        //this.image.setRotation(imgRotationSpeed);
+        SolarSystemTest.stage.addActor(this.image);
+}
+
+    //конструктор остальных тел
+    public Body(String textureName, float imgRotationSpeed, float distance, float diameter, float angle, float movingSpeed, Body parent) {
+        this.textureName = textureName;
+        this.imgRotationSpeed = imgRotationSpeed;
+        this.distance = distance;
+        this.diameter = diameter;
+        this.angle = angle;
+        this.movingSpeed = movingSpeed;
+        this.parent = parent;
+
+        parent.addChild(this);
+    }
+
+    private void addChild(Body child) {
+        childs.add(child);
+    }
+
+    public Body getParent() {
+        return parent;
+    }
+
+
 }
--- core/src/com/solar/game/SolarSystemTest.java
+++ core/src/com/solar/game/SolarSystemTest.java
@@ -3,35 +3,38 @@ package com.solar.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class SolarSystemTest extends ApplicationAdapter {
+	public static Stage stage;
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 600;
 	public static final String TITLE = "SolarSystemTest";
-
-	SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		stage = new Stage(new ScreenViewport());
+		Body Sun = new Body();
+
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act();
+		stage.draw();
+		/*
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+		*/
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		stage.dispose();
 	}
 }
